7de4d395d3499b8ad016cba50a6284ebac5a4b88
detely_ <detely.alex@gmail.com>
2019-09-04 00:56:58 +0300

    Added tree go

--- widgets/tree.go
+++ widgets/tree.go
@@ -13,8 +13,30 @@ var treeView *walk.TreeView
 var treeViewDeclaration TreeView
 
 type Directory struct {
+	parent   *Directory
 	name     string
 	children []*Directory
+}
+
+func (d Directory) Text() string {
+	return d.name
+}
+
+func (d Directory) Parent() walk.TreeItem {
+	if d.parent == nil {
+		// We can't simply return d.parent in this case, because the interface
+		// value then would not be nil.
+		return nil
+	}
+	return d.parent
+}
+
+func (d Directory) ChildCount() int {
+	return len(d.children)
+}
+
+func (d Directory) ChildAt(index int) walk.TreeItem {
+	return d.children[index]
 }
 
 type DirectoryTreeModel struct {
@@ -22,19 +44,19 @@ type DirectoryTreeModel struct {
 	roots []*Directory
 }
 
-func (d *DirectoryTreeModel) RootCount() int {
-	panic("implement me")
+func (m *DirectoryTreeModel) RootCount() int {
+	return len(m.roots)
 }
 
-func (d *DirectoryTreeModel) RootAt(index int) walk.TreeItem {
-	panic("implement me")
+func (m *DirectoryTreeModel) RootAt(index int) walk.TreeItem {
+	return m.roots[index]
 }
 
 var _ walk.TreeModel = new(DirectoryTreeModel)
 
 func CreateTreeView(size Size) TreeView {
 	if treeView == nil {
-		treeModel, err := NewDirectoryTreeModel()
+		treeModel, err := newDirectoryTreeModel()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -47,26 +69,17 @@ func CreateTreeView(size Size) TreeView {
 	return treeViewDeclaration
 }
 
-func NewDirectoryTreeModel() (*DirectoryTreeModel, error) {
+func newDirectoryTreeModel() (*DirectoryTreeModel, error) {
 	model := new(DirectoryTreeModel)
+	dir := newDirectory("a0", nil)
+	model.roots = append(model.roots, dir)
 
-	drives, err := walk.DriveNames()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, drive := range drives {
-		switch drive {
-		case "A:\\", "B:\\":
-			continue
-		}
-
-		model.roots = append(model.roots, NewDirectory(drive, nil))
-	}
+	_ = append(dir.children, newDirectory("a1", dir))
+	_ = append(dir.children, newDirectory("a2", dir))
 
 	return model, nil
 }
 
-func NewDirectory(name string, parent *Directory) *Directory {
+func newDirectory(name string, parent *Directory) *Directory {
 	return &Directory{name: name}
 }
