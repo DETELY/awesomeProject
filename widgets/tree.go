381db57879ab49e3215bc0b0a5bae4b5a7231823
bilynskyi.o <bilynskyi.o@dbbest.com>
2019-09-03 19:12:08 +0300

    add widgets

--- widgets/tree.go
+++ widgets/tree.go
@@ -5,6 +5,10 @@ import (
 	. "github.com/lxn/walk/declarative"
 )
 
+import (
+	"log"
+)
+
 var treeView *walk.TreeView
 var treeViewDeclaration TreeView
 
@@ -18,15 +22,51 @@ type DirectoryTreeModel struct {
 	roots []*Directory
 }
 
-var root DirectoryTreeModel
+func (d *DirectoryTreeModel) RootCount() int {
+	panic("implement me")
+}
+
+func (d *DirectoryTreeModel) RootAt(index int) walk.TreeItem {
+	panic("implement me")
+}
+
+var _ walk.TreeModel = new(DirectoryTreeModel)
 
 func CreateTreeView(size Size) TreeView {
 	if treeView == nil {
+		treeModel, err := NewDirectoryTreeModel()
+		if err != nil {
+			log.Fatal(err)
+		}
 		CurrentDialectPair = dialectPairs[0]
 		treeViewDeclaration = TreeView{
 			AssignTo: &treeView,
-			//Model:    root,
+			Model:    treeModel,
 		}
 	}
 	return treeViewDeclaration
+}
+
+func NewDirectoryTreeModel() (*DirectoryTreeModel, error) {
+	model := new(DirectoryTreeModel)
+
+	drives, err := walk.DriveNames()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, drive := range drives {
+		switch drive {
+		case "A:\\", "B:\\":
+			continue
+		}
+
+		model.roots = append(model.roots, NewDirectory(drive, nil))
+	}
+
+	return model, nil
+}
+
+func NewDirectory(name string, parent *Directory) *Directory {
+	return &Directory{name: name}
 }
